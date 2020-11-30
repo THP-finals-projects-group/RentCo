44c1e6fa00b44c465c5312f721328dc45bc60572
qdtrl <quentindtouroul@gmail.com>
2020-11-30 18:11:47 +0100

    devise validation model + views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+
+  validates_presence_of :firstname, :lastname, :phone_number
 end
+
