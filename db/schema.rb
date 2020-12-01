4901ece872e8b688ee200afd2511df1b38f358f2
Giaforax <doury.manuel@gmail.com>
2020-12-01 17:52:57 +0100

    Adding margin-top and padding-bottom for home_page_sign_in

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_11_30_165145) do
     t.string "firstname", null: false
     t.string "lastname", null: false
     t.string "phone_number", null: false
+    t.integer "role", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "confirmation_token"
