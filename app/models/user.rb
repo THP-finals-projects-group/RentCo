f73aacb212f918fd1198992454a4f45a98463fb3
qdtrl <quentindtouroul@gmail.com>
2020-12-04 09:51:56 +0100

    moddif form signup + create cases + background index

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
   validates               :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Entrez une adresse email valide" }
   validates               :firstname, length: {in: 3...64}, presence: true 
   validates               :lastname, length: {in: 3...64}, presence: true
-  validates               :phone_number, uniqueness: {case_sensitive: false}, format: { with: /\A(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})\z/, message: "Entrez un numéro de téléphone français valide" }
+  validates               :phone_number, uniqueness: {case_sensitive: false, message: "Ce numéro de téléphone est déjà utilisé" }, format: { with: /\A(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})\z/, message: "Entrez un numéro de téléphone français valide" }
   validates               :profession, presence: true
 
   def active_for_authentication? 
