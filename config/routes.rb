52a46c22d401212968bb51d10e22190c2218659b
Erudigit <duguet.vincent@outlook.fr>
2020-12-04 11:35:08 +0100

    adding controller and view rooms

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :cases
+  resources :cases do
+    resources :rooms, only: [:edit, :update]
+  end
 
   scope "/admin" do
     resources :users, only: [:index, :update]
