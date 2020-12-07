32989614cd05bd54d50ef080a25767a67b53165a
Queje <jeremyquerne@hotmail.fr>
2020-12-07 11:43:58 +0100

    fixing authenticate user in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class  UsersController < ApplicationController
+    before_action :set_user, only: [:show, :update]
+    before_action :authenticate_user!
 
     def index
         if params[:approved] == "false"
