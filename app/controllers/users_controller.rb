4961109ac5f2310fde7c5f7a9ed766948fb187e3
Queje <jeremyquerne@hotmail.fr>
2020-12-07 12:01:49 +0100

    private method set user in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,11 +11,11 @@ class  UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
+
     end
 
     def update
-        @user = User.find(params[:id])
+       
         if @user.update(approved: true)
             User.send_account_approval_mail(@user.email)
             respond_to do |format|
@@ -29,5 +29,11 @@ class  UsersController < ApplicationController
             end
         end
     end
+
+    private
+
+        def set_user
+            @user = User.find(params[:id])
+        end
 end
     
