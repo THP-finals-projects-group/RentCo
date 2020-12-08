662022ae8275d4b5028cd647c8cc900a7d55a19e
qdtrl <quentindtouroul@gmail.com>
2020-12-08 23:31:47 +0100

    everething but absolutly not addingVideoPreview

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -22,25 +22,36 @@ class CasesController < ApplicationController
         send_data pdf, filename: "#{@case.id}_dossier"
     end
 
+    def generate_compute
+        ComputeCalcul.compute_finals_calculs(params[:id])
+        redirect_to case_path(params[:id])
+    end
+
     def new
         @case = Case.new
+        @case.rooms.new
         @s_button_submit = "Créer le dossier"
         @s_title_document = "Création d'un nouveau dossier"
     end
 
-    def create
+    def create   
         @case = User.find(current_user.id).cases.new(cases_params)
-
+        if current_user.administrator?
+            @case.save
+            rooms = params[:case][:rooms_attributes]
+            rooms.each do |hash|
+                (hash[1].values_at("_destroy")[0] == "false" && !(hash[1].values_at("rent_monthly")[0] == "0")) ? rent = hash[1].values_at("rent_monthly")[0] : next 
+                @case.rooms.create!(rent_monthly: rent.to_i)
+            end
+            @case.new_rooms_count = @case.rooms.length
+        end
         if @case.save
-            if current_user.administrator? 
-                @case.new_rooms_count.times do
-                    Room.create(case_id: @case.id)
-                end
-                flash[:info]="Veuillez remplir le prix des chambres"
-                redirect_to edit_case_room_path(@case.id, case_id: @case.id)
+            if current_user.administrator?
+                flash[:success] = 'Votre dossier à bien été créé.'
+                redirect_to case_path(@case.id)
             else
                 flash[:success] = 'Votre dossier à bien été ouvert.'
-                redirect_to root_path
+                redirect_to case_path(@case.id)
             end
         else 
             flash.now[:alert] = "Le dossier n'a pas pu être enregistré : #{@case.errors.messages}"
@@ -64,15 +75,17 @@ class CasesController < ApplicationController
     def update 
         @case = Case.find(params[:id])
         if current_user.administrator? 
-            @case.rooms.each do |room_completed|
-                room_completed.destroy
-            end
             @case.update(cases_params)
-            @case.new_rooms_count.times do |room|
-                Room.create(case_id: @case.id)
+            @case.rooms.destroy_all
+            rooms = params[:case][:rooms_attributes]
+            rooms.each do |hash|
+                (hash[1].values_at("_destroy")[0] == "false" && !(hash[1].values_at("rent_monthly")[0] == "0")) ? rent = hash[1].values_at("rent_monthly")[0] : next 
+                @case.rooms.create!(rent_monthly: rent.to_i)
             end
+            @case.new_rooms_count = @case.rooms.length
+            @case.save
             flash[:success] =  "Le dossier à était mis à jour!"
-            redirect_to edit_case_room_path(@case.id, case_id: @case.id)
+            redirect_to case_path(@case.id)
         else
             @case.update(cases_params)
             flash[:success] =  "Le dossier à était mis à jour!"
@@ -123,10 +136,22 @@ class CasesController < ApplicationController
     
         def cases_params
             if current_user.administrator?
-                params.require(:case).permit(:is_confirmed, :title, :case_reference, :visit_date, :street_number, :street_name, :city, :zipcode, :old_project, :old_surface, :old_type, :old_rooms_count, :seller_price, :estimated_negociation, :property_taxes, :renovation_union, :notary_charges, :union_charges_cost, :heater_cost, :water_cost, :electricity_cost, :common_charges_cost, :agency_charges, :physical_description, :geographical_description, :potential_description, :renovation_demolition_cost, :renovation_preparation_cost, :renovation_carpentry_cost, :renovation_plastering_cost, :renovation_electricity_cost, :renovation_plumbing_cost, :renovation_wall_ceiling_cost, :renovation_painting_cost, :renovation_flooring_cost, :renovation_kitchen_cost, :renovation_furniture_cost, :renovation_facade_cost, :renovation_security_cost, :renovation_masonry_cost, :renovation_covering_cost, :new_type, :new_project, :new_surface, :new_rooms_count, :month_count, videos: [])
+                params.require(:case).permit( :is_confirmed, :title, :case_reference, :visit_date, :street_number, :street_name, :city, :zipcode, :old_project, :old_surface, :old_type, :old_rooms_count, :seller_price, :estimated_negociation, :property_taxes, :renovation_union, :notary_charges, :union_charges_cost, :heater_cost, :water_cost, :electricity_cost, :common_charges_cost, :agency_charges, :physical_description, :geographical_description, :potential_description, :renovation_demolition_cost, :renovation_preparation_cost, :renovation_carpentry_cost, :renovation_plastering_cost, :renovation_electricity_cost, :renovation_plumbing_cost, :renovation_wall_ceiling_cost, :renovation_painting_cost, :renovation_flooring_cost, :renovation_kitchen_cost, :renovation_furniture_cost, :renovation_facade_cost, :renovation_security_cost, :renovation_masonry_cost, :renovation_covering_cost, :new_type, :new_project, :new_surface, :new_rooms_count, :month_count, videos: [])
             else
                 params.require(:case).permit(:title, :case_reference, :visit_date, :street_number, :street_name, :city, :zipcode, :old_project, :old_surface, :old_type, :old_rooms_count, :seller_price, :estimated_negociation, :property_taxes, :renovation_union, :notary_charges, :union_charges_cost, :heater_cost, :water_cost, :electricity_cost, :common_charges_cost, :agency_charges, :physical_description, :geographical_description, :potential_description, videos: [])
             end
         end
+        # :rooms_attributes [[]=>["rent_monthly", "_destroy"]],
+        # def params_rooms(c)
+        #     c.rooms.each do |room_completed|
+        #         room_completed.destroy
+        #     end
+        #     rooms = params[:case][:rooms_attributes]
+        #     rooms.each do |hash|
+        #         hash[1].values_at("rent_monthly").empty? ? rent = hash[1].values_at("rent_monthly") : next
+        #         c.rooms.new(rent_monthly: rent.to_i)
+        #     end
+        #     c
+        # end
 
 end
