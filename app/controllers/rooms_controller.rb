0b16fd47034436b5a117d3e790cf76bf0a2de770
qdtrl <quentindtouroul@gmail.com>
2020-12-08 16:03:09 +0100

    save

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,17 +2,18 @@ class RoomsController < ApplicationController
     before_action :authenticate_user!
 
     def edit
-        @room = Room.find(params[:id])
+        @case = Case.find(params[:id])
+        @rooms = @case.rooms
+        @case_rooms = Room.where(case_id: params[:id])
     end
 
     def update 
         
         @case_rooms = Room.where(case_id: params[:id])
-        binding.pry
-        params.require(:case).permit(:onsenfous)
         @case_rooms.each do |room| 
             room.update(rent_monthly: params[room.rent_monthly.to_s].to_i)
         end 
+
         ComputeCalcul.compute_finals_calculs(params[:id])
         redirect_to case_path(params[:id])
     end
